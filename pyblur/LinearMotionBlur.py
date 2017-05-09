66905b99efa750f87102ca437632dab7019e2bb6
HoiweChenge <chmtt123@163.com>
2017-05-09 22:39:46 +0800

    remove dependency on LineDictionary. changed the implentation closet to matlab version--fspecial

--- pyblur/LinearMotionBlur.py
+++ pyblur/LinearMotionBlur.py
@@ -1,67 +1,34 @@
 # -*- coding: utf-8 -*-
 import math
 import numpy as np
-from PIL import Image
-from scipy.signal import convolve2d
-from skimage.draw import line
+import cv2
 
-from .LineDictionary import LineDictionary
 
-lineLengths =[3,5,7,9]
-lineTypes = ["full", "right", "left"]
 
-lineDict = LineDictionary()
+def LinearMotionBlur_random(img,length_span, theta_span):
+    if type([]) != type(length_span):
+        raise Exception("length_span should be list")
+    if type([]) != type(theta_span):
+        raise Exception("length_span should be list")
+    if theta_span.max() >= 180:
+        raise Exception('max angle should be less than 180')
 
-def LinearMotionBlur_random(img):
-    lineLengthIdx = np.random.randint(0, len(lineLengths))
-    lineTypeIdx = np.random.randint(0, len(lineTypes)) 
-    lineLength = lineLengths[lineLengthIdx]
-    lineType = lineTypes[lineTypeIdx]
-    lineAngle = randomAngle(lineLength)
-    return LinearMotionBlur(img, lineLength, lineAngle, lineType)
+    length = np.random.randint(length_span)
+    angle = np.random.randint(theta_span)
+    return LinearMotionBlur(img, lineLength, angle)
 
-def LinearMotionBlur(img, dim, angle, linetype):
+def LinearMotionBlur(img, dim, angle):
     imgarray = np.array(img, dtype="float32")
-    kernel = LineKernel(dim, angle, linetype)
-    convolved = convolve2d(imgarray, kernel, mode='same', fillvalue=255.0).astype("uint8")
-    img = Image.fromarray(convolved)
-    return img
+    kernel = getBlurKernel(dim, angle)
+    BlurImg = cv2.filter2D(imgarray, -1, kernel).astype("uint8")
+    return BlurImg
 
-def LineKernel(dim, angle, linetype):
-    kernelwidth = dim
-    kernelCenter = int(math.floor(dim/2))
-    angle = SanitizeAngleValue(kernelCenter, angle)
-    kernel = np.zeros((kernelwidth, kernelwidth), dtype=np.float32)
-    lineAnchors = lineDict.lines[dim][angle]
-    if(linetype == 'right'):
-        lineAnchors[0] = kernelCenter
-        lineAnchors[1] = kernelCenter
-    if(linetype == 'left'):
-        lineAnchors[2] = kernelCenter
-        lineAnchors[3] = kernelCenter
-    rr,cc = line(lineAnchors[0], lineAnchors[1], lineAnchors[2], lineAnchors[3])
-    kernel[rr,cc]=1
-    normalizationFactor = np.count_nonzero(kernel)
-    kernel = kernel / normalizationFactor        
+def getBlurKernel(length, theta):
+    if length % 2 == 0:
+        raise Exception("length should be odd!", length)
+    kernel = np.zeros([length, length])
+    kernel[math.floor(length/2),:] = 1
+    mask = cv2.getRotationMatrix2D((length/2,length/2),theta,1)
+    kernel = cv2.warpAffine(kernel, mask, (length, length))
+    kernel = kernel / kernel.sum()
     return kernel
-
-def SanitizeAngleValue(kernelCenter, angle):
-    numDistinctLines = kernelCenter * 4
-    angle = math.fmod(angle, 180.0)
-    validLineAngles = np.linspace(0,180, numDistinctLines, endpoint = False)
-    angle = nearestValue(angle, validLineAngles)
-    return angle
-
-def nearestValue(theta, validAngles):
-    idx = (np.abs(validAngles-theta)).argmin()
-    return validAngles[idx]
-
-def randomAngle(kerneldim):
-    kernelCenter = int(math.floor(kerneldim/2))
-    numDistinctLines = kernelCenter * 4
-    validLineAngles = np.linspace(0,180, numDistinctLines, endpoint = False)
-    angleIdx = np.random.randint(0, len(validLineAngles))
-    return int(validLineAngles[angleIdx])
-
-def drawKernel(kernel):
-    pass
